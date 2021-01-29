1fa60b7bee982af024bb4efe0d6dbbcb9a6a43a6
Luke <luke_chang@fittech.com.tw>
2021-01-29 17:27:34 +0800

    version 1, 用timer寫狀態機

--- Claw_Machine/Form1.Designer.cs
+++ Claw_Machine/Form1.Designer.cs
@@ -29,11 +29,169 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.lbPlayTimes = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.plGameZone = new System.Windows.Forms.Panel();
+            this.pbObject2 = new System.Windows.Forms.PictureBox();
+            this.pbObject1 = new System.Windows.Forms.PictureBox();
+            this.pbClaw = new System.Windows.Forms.PictureBox();
+            this.plBlackButton = new System.Windows.Forms.Panel();
+            this.pbObject0 = new System.Windows.Forms.PictureBox();
+            this.MainState = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.plGameZone.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbObject2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbObject1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbClaw)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbObject0)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbTime);
+            this.panel1.Controls.Add(this.lbPlayTimes);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 300);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 53);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Location = new System.Drawing.Point(217, 16);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(70, 21);
+            this.lbTime.TabIndex = 4;
+            this.lbTime.Text = "Time : 0";
+            // 
+            // lbPlayTimes
+            // 
+            this.lbPlayTimes.AutoSize = true;
+            this.lbPlayTimes.Location = new System.Drawing.Point(81, 16);
+            this.lbPlayTimes.Name = "lbPlayTimes";
+            this.lbPlayTimes.Size = new System.Drawing.Size(110, 21);
+            this.lbPlayTimes.TabIndex = 3;
+            this.lbPlayTimes.Text = "PlayTimes : 0";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 9);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(58, 34);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // plGameZone
+            // 
+            this.plGameZone.Controls.Add(this.pbObject2);
+            this.plGameZone.Controls.Add(this.pbObject1);
+            this.plGameZone.Controls.Add(this.pbClaw);
+            this.plGameZone.Controls.Add(this.plBlackButton);
+            this.plGameZone.Controls.Add(this.pbObject0);
+            this.plGameZone.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plGameZone.Location = new System.Drawing.Point(0, 0);
+            this.plGameZone.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.plGameZone.Name = "plGameZone";
+            this.plGameZone.Size = new System.Drawing.Size(500, 300);
+            this.plGameZone.TabIndex = 1;
+            // 
+            // pbObject2
+            // 
+            this.pbObject2.Image = ((System.Drawing.Image)(resources.GetObject("pbObject2.Image")));
+            this.pbObject2.Location = new System.Drawing.Point(257, 224);
+            this.pbObject2.Name = "pbObject2";
+            this.pbObject2.Size = new System.Drawing.Size(77, 60);
+            this.pbObject2.TabIndex = 4;
+            this.pbObject2.TabStop = false;
+            // 
+            // pbObject1
+            // 
+            this.pbObject1.BackColor = System.Drawing.Color.Transparent;
+            this.pbObject1.Image = ((System.Drawing.Image)(resources.GetObject("pbObject1.Image")));
+            this.pbObject1.Location = new System.Drawing.Point(131, 212);
+            this.pbObject1.Name = "pbObject1";
+            this.pbObject1.Size = new System.Drawing.Size(75, 72);
+            this.pbObject1.TabIndex = 3;
+            this.pbObject1.TabStop = false;
+            // 
+            // pbClaw
+            // 
+            this.pbClaw.Image = ((System.Drawing.Image)(resources.GetObject("pbClaw.Image")));
+            this.pbClaw.Location = new System.Drawing.Point(0, 0);
+            this.pbClaw.Name = "pbClaw";
+            this.pbClaw.Size = new System.Drawing.Size(74, 74);
+            this.pbClaw.TabIndex = 0;
+            this.pbClaw.TabStop = false;
+            // 
+            // plBlackButton
+            // 
+            this.plBlackButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.plBlackButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.plBlackButton.Location = new System.Drawing.Point(0, 285);
+            this.plBlackButton.Name = "plBlackButton";
+            this.plBlackButton.Size = new System.Drawing.Size(500, 15);
+            this.plBlackButton.TabIndex = 2;
+            // 
+            // pbObject0
+            // 
+            this.pbObject0.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbObject0.Location = new System.Drawing.Point(88, 212);
+            this.pbObject0.Name = "pbObject0";
+            this.pbObject0.Size = new System.Drawing.Size(14, 73);
+            this.pbObject0.TabIndex = 5;
+            this.pbObject0.TabStop = false;
+            // 
+            // MainState
+            // 
+            this.MainState.Enabled = true;
+            this.MainState.Tick += new System.EventHandler(this.MainState_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 353);
+            this.Controls.Add(this.plGameZone);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.plGameZone.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbObject2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbObject1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbClaw)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbObject0)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel plGameZone;
+        private System.Windows.Forms.PictureBox pbClaw;
+        private System.Windows.Forms.Panel plBlackButton;
+        private System.Windows.Forms.Label lbPlayTimes;
+        private System.Windows.Forms.Timer MainState;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.PictureBox pbObject1;
+        private System.Windows.Forms.PictureBox pbObject2;
+        private System.Windows.Forms.PictureBox pbObject0;
     }
 }
 
--- Claw_Machine/Form1.cs
+++ Claw_Machine/Form1.cs
@@ -12,9 +12,274 @@ namespace Claw_Machine
 {
     public partial class Form1 : Form
     {
+        public enum State1 { Idle, Start, Initialize, DetectTime, Move, Grab }
+        public List<double> Object_X = new List<double>();
+        public List<double> Object_Y = new List<double>();
+        State1 s1;
+        int s2 = 0;
+        bool s3 = false;
+        int PlayTimes = 0;
+        bool bStart = false;
+        bool bLeft = false, bRight = false, bDown = false;
+        int TimeLimit = 10;
+        int time_start;
+        int StepAxis = 10;
+        int condition = 10;
+
+        int number;
+
         public Form1()
         {
             InitializeComponent();
+
+            Object_X.Add(pbObject1.Location.X);
+            Object_Y.Add(pbObject1.Location.Y);
+
+            Object_X.Add(pbObject2.Location.X);
+            Object_Y.Add(pbObject2.Location.Y);
+
+        }
+
+        void Run()
+        {
+            while (s3 == true)
+            {
+            }
+        }
+
+        private void MainState_Tick(object sender, EventArgs e)
+        {
+            
+            switch (s1)
+            {
+                case State1.Idle:
+                    SetLabel(lbTime, $"Time : {0}");
+                    btnStart.Enabled = true;
+                    if (bStart == true)
+                    {
+                        s1 = State1.Start;
+                    }
+                    break;
+
+                case State1.Start:
+                    PlayTimes++;
+                    SetLabel(lbPlayTimes, $"PlayTimes : {PlayTimes}");
+                    s1 = State1.Move;
+                    break;
+
+                case State1.Move:
+                    SetLabel(lbTime, $"Time : {(Environment.TickCount - time_start) / 1000}");
+                    s1 = State1.DetectTime;
+
+                    if (bRight == true)
+                        MoveXAxis(pbClaw, StepAxis);
+                    else if (bLeft == true)
+                        MoveXAxis(pbClaw, -1*StepAxis);
+                    else if (bDown == true)
+                        s1 = State1.Grab;
+
+                    break;
+
+                case State1.Grab:
+                    switch (s2)
+                    {
+                        case 0:
+                            if (pbClaw.Location.Y + StepAxis <= plGameZone.Size.Height - pbClaw.Size.Height - plBlackButton.Size.Height)
+                            {
+                                pbClaw.Top += StepAxis;
+                                JudgeClawTouchObject(pbClaw, pbObject0, 0, ref number, ref s2);
+                                JudgeClawTouchObject(pbClaw, pbObject1, condition, ref number, ref s2);
+                                JudgeClawTouchObject(pbClaw, pbObject2, condition, ref number, ref s2);
+                            }
+                            else
+                                s2 = 10;
+                            break;
+
+                        case 10:
+                            if (pbClaw.Location.Y > 0)
+                            {
+                                pbClaw.Top += -1 * StepAxis;
+                                GrabObject_Y(number, ref s2);
+                            }
+                                
+                                
+                            else
+                                s2 = 20;
+                            break;
+
+                        case 20:
+                            if (pbClaw.Location.X > 0)
+                            {
+                                pbClaw.Left += -1 * StepAxis;
+                                GrabObject_X(number, ref s2);
+                            }
+                            else if (number == 0) // 沒夾到東西
+                            {
+                                s2 = 0;
+                                bStart = false;
+                                s1 = State1.Initialize;
+                            }
+                            else
+                            {
+                                GrabRelease(ref number);
+                            }
+                            break;
+                    }
+                    break;
+
+
+                case State1.Initialize:
+                    if (pbClaw.Location.X != 0 && pbClaw.Location.Y != 0)
+                    {
+                        pbClaw.Left += -StepAxis;
+                        pbClaw.Top += -StepAxis;
+                    }
+                    s1 = State1.Idle;
+                    number = 0;
+                    break;
+
+                case State1.DetectTime:
+                    if(Environment.TickCount - time_start>= TimeLimit * 1000)
+                        s1 = State1.Grab;
+                    else
+                        s1 = State1.Move;
+                    break;
+                    
+            }
+
+        }
+
+        //--------------------------------------------
+        void JudgeClawTouchObject(PictureBox pbClaw, PictureBox pbObject, int cdt, ref int number, ref int state)
+        {
+            double Claw_Width = pbClaw.Size.Width;
+            double Claw_Height = pbClaw.Size.Height;
+            double Claw_X = pbClaw.Location.X;
+            double Claw_Y = pbClaw.Location.Y;
+
+            double Object_Width = pbObject.Size.Width;
+            double Object_Height = pbObject.Size.Height;
+            double Object_X = pbObject.Location.X;
+            double Object_Y = pbObject.Location.Y;
+
+            double Claw_Fact_X = Claw_X + Claw_Width * 0.5;
+            double Claw_Fact_Y = Claw_Y + Claw_Height;
+
+            double Object_upper_X1 = Object_X;
+            double Object_upper_X2 = Object_X + Object_Width;
+            double Object_Fact_Y = Object_Y;
+
+            if (Claw_Fact_X - cdt >= Object_upper_X1 & Claw_Fact_X + cdt <= Object_upper_X2 & Claw_Fact_Y >= Object_Fact_Y)
+            {
+                number = Convert.ToInt32( pbObject.Name.Substring(8,1) );
+                state = 10;
+                return;
+            }
+        }
+
+        void GrabObject_Y(int number, ref int state)
+        {
+            if (number == 1)
+                pbObject1.Top += -1 * StepAxis;
+            else if (number == 2)
+                pbObject2.Top += -1 * StepAxis;
+        }
+
+        void GrabObject_X(int number, ref int state)
+        {
+            if (number == 1)
+                pbObject1.Left += -1 * StepAxis;
+            else if (number == 2)
+                pbObject2.Left += -1 * StepAxis;
+        }
+
+        void GrabRelease(ref int number)
+        {
+            if (number == 1)
+            {
+                if (pbObject1.Location.Y != Object_Y[number-1])
+                    pbObject1.Top += StepAxis;
+                else if (pbObject1.Location.Y == Object_Y[number-1])
+                    number = 0;
+            }
+            else if (number == 2)
+            {
+                if (pbObject2.Location.Y != Object_Y[number - 1])
+                    pbObject2.Top += StepAxis;
+                else if (pbObject2.Location.Y == Object_Y[number - 1])
+                    number = 0;
+            }
+        }
+
+        void MoveXAxis(PictureBox pb,  int Speed)
+        {
+            if (pbClaw.Location.X + Speed >= 0 && pbClaw.Location.X + Speed <= plGameZone.Size.Width - pbClaw.Size.Width)
+                pb.Left += Speed;
+        }
+
+        //--------- for 委派 -------------------------------------------------
+        public delegate void SetLabelTextCallback(Label lb, string text);
+
+        public void SetLabel(Label lb, string text)
+        {
+            if (lb.InvokeRequired)
+            {
+                SetLabelTextCallback d = new SetLabelTextCallback(SetLabel);
+                Invoke(d, new object[] { lb, text });
+            }
+            else
+                lb.Text = text;
+        }
+        
+
+
+        //---------- UI -------------------------------------------------------
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            bStart = true;
+            s1 = State1.Start;
+            time_start = Environment.TickCount;
+            btnStart.Enabled = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                bLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                bRight = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                bDown = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                bLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                bRight = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                bDown = false;
+            }
+        }
+    }
+
+    public class abc
+    {
+        void a(PictureBox pb)
+        {
+            pb.Left += 5;
         }
     }
 }
